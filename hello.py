38ca94513ba8da2abb2bc284917b469602484598
ady2303 <2303singh.aditya@gmail.com>
2024-01-31 17:19:11 -0500

    Added transcript getting functionality

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print("Namaste Dunia")
+from youtube_transcript_api import YouTubeTranscriptApi
+
+url = "https://www.youtube.com/watch?v=2TL3DgIMY1g"
+
+video_id = url.replace('https://www.youtube.com/watch?v=', '')
+print(video_id)
+
+transcript = YouTubeTranscriptApi.get_transcript(video_id)
+
+output = ''
+
+for x in transcript:
+    sentence = x['text']
+    output += f' {sentence}\n'
+
+print(output)
